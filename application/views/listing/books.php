b4717b0f68927c2e2dc859b31c2d0b2db16c04bc
suresh07 <soori_rozee@yahoo.com>
2018-10-03 18:32:45 +0530

    Missing blank toc added

--- application/views/listing/books.php
+++ application/views/listing/books.php
@@ -2,7 +2,7 @@
 	<h2>Collection of Books</h2>
 	<div class="row">
 <?php foreach ($data as $file) { ?>
-        <div class="col-md-3 booksCollection">
+        <div class="col-md-2 booksCollection">
             <a href="<?=BASE_URL?>listing/toc/<?= $file ?>"><img src="<?=IMAGE_URL?>books/<?=$file?>.jpg" alt="Books images" /></a>
         </div>
 <?php } ?>
