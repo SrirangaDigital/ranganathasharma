60dfd29b54bddb34f9bfafda40fd21b34aa5c7a6
kishorssk <kishor.sathigrama@gmail.com>
2019-01-04 19:20:00 +0530

    book category work in progress

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -1,8 +1,44 @@
-<div class="container-fluid banner firstDiv">
-    <div class="row justify-content-center overlay">
-        <div class="col-md-12 align-self-center">
-        	<h1>Coming soon</h1>
-    	</div>
+<div class="container-fluid flat-page firstDiv">
+	<div class="row justify-content-center overlay">
+		<div class="col-md-12 align-self-center">
+			<div class="container">
+				<h2>ಕೃತಿಗಳು</h2><br />
+				<div class="row">
+					<div class="card-columns">
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text"><a href="<?=BASE_URL?>krutigalu/001">ಸ್ವತಂತ್ರ ಗ್ರಂಥಗಳು</a></p>
+							</div>
+						</div>
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text"><a href="<?=BASE_URL?>krutigalu/002">ಅನುವಾದ ಗ್ರಂಥಗಳು</a></p>
+							</div>
+						</div>
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text">ವ್ಯಾಖ್ಯಾನ ಗ್ರಂಥಗಳು</p>
+							</div>
+						</div>
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text">ಸಂಪಾದಿತ ಗ್ರಂಥಗಳು</p>
+							</div>
+						</div>  
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text">ಸಹಕರ್ತೃ ಗ್ರಂಥಗಳು</p>
+							</div>
+						</div>
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text">ಲೇಖನಗಳು</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		<a class="homepage scrollMore goTo" data-destination="#wordOfTheDay"><span></span>&nbsp;</a>
 	</div>
 </div>
---
+++ application/views/flat/krutigalu/001/index.php
@@ -0,0 +1,110 @@
+<div class="container-fluid flat-page firstDiv">
+	<div class="row justify-content-center overlay">
+		<div class="col-md-12 align-self-center">
+			<div class="container">
+				<h2>ಸ್ವತಂತ್ರ ಗ್ರಂಥಗಳು</h2>
+				<br>
+				<!-- Nav tabs -->
+				<ul class="nav nav-tabs" role="tablist">
+					<li class="nav-item">
+						<a class="nav-link active" data-toggle="tab" href="#kannada">ಕನ್ನಡ ಭಾಷೆ</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" data-toggle="tab" href="#sanskrit">संस्कृतभाषा</a>
+					</li>
+				</ul>
+
+				<!-- Tab panes -->
+				<div class="tab-content">
+					<div id="kannada" class="container tab-pane active"><br>
+						<div class="row">
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/042"><img src="<?=IMAGE_URL?>books/042.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/041"><img src="<?=IMAGE_URL?>books/041.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/043"><img src="<?=IMAGE_URL?>books/043.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/045"><img src="<?=IMAGE_URL?>books/045.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/046"><img src="<?=IMAGE_URL?>books/046.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/048"><img src="<?=IMAGE_URL?>books/048.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/040"><img src="<?=IMAGE_URL?>books/040.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/034"><img src="<?=IMAGE_URL?>books/034.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/025"><img src="<?=IMAGE_URL?>books/025.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/033"><img src="<?=IMAGE_URL?>books/033.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/026"><img src="<?=IMAGE_URL?>books/026.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/027"><img src="<?=IMAGE_URL?>books/027.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/032"><img src="<?=IMAGE_URL?>books/032.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/036"><img src="<?=IMAGE_URL?>books/036.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/037"><img src="<?=IMAGE_URL?>books/037.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/030"><img src="<?=IMAGE_URL?>books/030.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/028"><img src="<?=IMAGE_URL?>books/028.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/035"><img src="<?=IMAGE_URL?>books/035.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/038"><img src="<?=IMAGE_URL?>books/038.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/039"><img src="<?=IMAGE_URL?>books/039.jpg" alt="Books images" /></a>
+							</div>
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/029"><img src="<?=IMAGE_URL?>books/029.jpg" alt="Books images" /></a>
+							</div>
+						</div>
+					</div>
+					<div id="sanskrit" class="container tab-pane fade"><br>
+						<div class="row">
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/072"><img src="<?=IMAGE_URL?>books/072.jpg" alt="Books images" /></a>
+							</div>	
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/091"><img src="<?=IMAGE_URL?>books/091.jpg" alt="Books images" /></a>
+							</div>	
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/055"><img src="<?=IMAGE_URL?>books/055.jpg" alt="Books images" /></a>
+							</div>	
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/087"><img src="<?=IMAGE_URL?>books/087.jpg" alt="Books images" /></a>
+							</div>	
+							<div class="col-md-2 booksCollection">
+								<a href="<?=BASE_URL?>listing/toc/089"><img src="<?=IMAGE_URL?>books/089.jpg" alt="Books images" /></a>
+							</div>
+						</div>	
+					</div>
+				</div>
+			</div>
+		</div>
+		<a class="homepage scrollMore goTo" data-destination="#wordOfTheDay"><span></span>&nbsp;</a>
+	</div>
+</div>
