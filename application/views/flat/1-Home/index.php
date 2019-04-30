fcd71597bab19611c20e739207e961832c5466d4
Suresh <soori_rozee@yahoo.com>
2019-04-30 15:44:22 +0530

    Carousel images added

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -1,49 +1,50 @@
-<div class="container-fluid flat-page firstDiv">
+<div class="container-fluid flat-page">
 	<div class="row justify-content-center overlay">
 		<div class="col-md-12 align-self-center">
 			<div class="container">
-				<h2>ಕೃತಿಗಳು</h2><br />
-				<div class="row">
-					<div class="card-columns">
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>krutigalu/001">
-								<div class="card-body text-center">ಸ್ವತಂತ್ರ ಗ್ರಂಥಗಳು</div>
-							</a>
+				<div id="carousel" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carousel" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel" data-slide-to="1"></li>
+						<li data-target="#carousel" data-slide-to="2"></li>
+						<li data-target="#carousel" data-slide-to="3"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block" src="<?=IMAGE_URL?>carousel/carousel1.jpg" alt="ಕರ್ನಾಟಕ ಸಂಸ್ಕೃತ ವಿಶ್ವವಿದ್ಯಾಲಯ ಶ್ರೀಯುತ ಶರ್ಮರಿಗೆ ನೀಡಿದ ಗೌರವ ಡಾಕ್ಟರೇಟ್ ಪ್ರಶಸ್ತಿ ಸಮಾರಂಭ">
+							<div class="carousel-caption pos-2-3-left scheme-dark">
+								<p class="caption-content">ಕರ್ನಾಟಕ ಸಂಸ್ಕೃತ ವಿಶ್ವವಿದ್ಯಾಲಯ ಶ್ರೀಯುತ ಶರ್ಮರಿಗೆ ನೀಡಿದ ಗೌರವ ಡಾಕ್ಟರೇಟ್ ಪ್ರಶಸ್ತಿ ಸಮಾರಂಭ</p>
+							</div>
 						</div>
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>krutigalu/002">
-								<div class="card-body text-center">ಅನುವಾದ ಗ್ರಂಥಗಳು</div>
-							</a>
+						<div class="carousel-item">
+							<img class="d-block" src="<?=IMAGE_URL?>carousel/carousel2.jpg" alt="ಧರ್ಮಸ್ಥಳದ ಧರ್ಮಾಧಿಕಾರಿ ಡಾ|| ವೀರೇಂದ್ರ ಹೆಗ್ಗಡೆಯವರು ಪುರಸ್ಕರಿಸುತ್ತಿರುವುದು">
+							<div class="carousel-caption pos-2-3-left scheme-dark">
+								<p class="caption-content">ಧರ್ಮಸ್ಥಳದ ಧರ್ಮಾಧಿಕಾರಿ ಡಾ॥ ವೀರೇಂದ್ರ ಹೆಗ್ಗಡೆಯವರು ಪುರಸ್ಕರಿಸುತ್ತಿರುವುದು</p>
+							</div>
 						</div>
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>krutigalu/003">
-								<div class="card-body text-center">ವ್ಯಾಖ್ಯಾನ ಗ್ರಂಥಗಳು</div>
-							</a>
+						<div class="carousel-item">
+							<img class="d-block" src="<?=IMAGE_URL?>carousel/carousel3.jpg" alt="ಡಿ.ವಿ.ಜಿ. ಬಳಗದಿಂದ ಸಮ್ಮಾನ">
+							<div class="carousel-caption pos-2-3-left scheme-dark">
+								<p class="caption-content">ಕರ್ನಾಟಕ ಸಂಸ್ಕೃತ ವಿಶ್ವವಿದ್ಯಾಲಯ ಶ್ರೀಯುತ ಶರ್ಮರಿಗೆ ನೀಡಿದ ಗೌರವ ಡಾಕ್ಟರೇಟ್ ಪ್ರಶಸ್ತಿ ಸಮಾರಂಭ</p>
+							</div>
 						</div>
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>krutigalu/004">
-								<div class="card-body text-center">ಸಂಪಾದಿತ ಗ್ರಂಥಗಳು</div>
-							</a>
-						</div>  
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>krutigalu/005">
-								<div class="card-body text-center">ಸಹಕರ್ತೃ ಗ್ರಂಥಗಳು</div>
-							</a>
-						</div>
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>krutigalu/006">
-								<div class="card-body text-center">ಲೇಖನಗಳು</div>
-							</a>
-						</div>
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>krutigalu/007">
-								<div class="card-body text-center">ಇತರ ಗ್ರಂಥಗಳು</div>
-							</a>
+						<div class="carousel-item">
+							<img class="d-block" src="<?=IMAGE_URL?>carousel/carousel4.jpg" alt="ಅಮೇರಿಕಾದ ವೈಟ್ ಹೌಸ್ ಮುಂಭಾಗದಲ್ಲಿ">
+							<div class="carousel-caption pos-2-3-left scheme-dark">
+								<p class="caption-content">ಅಮೇರಿಕಾದ ವೈಟ್ ಹೌಸ್ ಮುಂಭಾಗದಲ್ಲಿ ಡಾ॥ ವಿದ್ವಾನ್ ಎನ್. ರಂಗನಾಥಶರ್ಮಾ</p>
+							</div>
 						</div>
 					</div>
+					<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
 				</div>
 			</div>
 		</div>
-		<a class="homepage scrollMore goTo" data-destination="#wordOfTheDay"><span></span>&nbsp;</a>
 	</div>
 </div>
