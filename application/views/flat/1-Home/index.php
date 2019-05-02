fb534804b8611be992bd519ff09dedcf42738e83
Suresh <soori_rozee@yahoo.com>
2019-05-02 12:27:21 +0530

    Home page UI fixing

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -1,7 +1,7 @@
 <div class="container-fluid flat-page">
-	<div class="row justify-content-center overlay">
-		<div class="col-md-12 align-self-center">
-			<div class="container">
+	<div class="row justify-content-center">
+		<div class="col-md-12">
+			<div class="container padding-zero">
 				<div id="carousel" class="carousel slide" data-ride="carousel">
 					<ol class="carousel-indicators">
 						<li data-target="#carousel" data-slide-to="0" class="active"></li>
@@ -48,3 +48,50 @@
 		</div>
 	</div>
 </div>
+<div class="container">
+	<div class="row small-bio mt-5">
+		<div class="col-md-12 subtitle">
+			<h4>ರಂಗನಾಥಶರ್ಮಾ ಸಣ್ಣ ಪರಿಚಯ</h4>
+		</div>
+		<div class="col-md-8">
+			ಪುಣ್ಯಭೂಮಿ – ತಪೋಭೂಮಿ – ಕರ್ಮಭೂಮಿಯೆನಿಸಿದ ಭರತಭೂಮಿಯಲ್ಲಿ ಅದೆಷ್ಟೋ ಜ್ಞಾನಿಗಳು, ವಿಜ್ಞಾನಿಗಳು, ಅವಧೂತರು, ತಪಸ್ವಿಗಳು, ಸಾಧಕರು, ವಿದ್ವಾಂಸರು, ಪುಣ್ಯಾತ್ಮರು ಅವತರಿಸಿ ಜಗಕುಪಕರಿಸಿ ಬದುಕನ್ನು ಸಾರ್ಥಕವನ್ನಾಗಿಸಿದ್ದಾರೆ. ಇಂತಹ ಪರಂಪರೆಯಲ್ಲಿ ಅವತರಿಸಿದ ಸಾಧಕೋತ್ತಮ ವಿದ್ವಾಂಸರು ವಿದ್ವಾನ್ ಎನ್ ರಂಗನಾಥಶರ್ಮರು. ಸಹೃದಯತೆ-ಸದ್ಗುಣ-ಪಾಂಡಿತ್ಯಗಳ ಸಂಗಮಕ್ಕೊಂದು ಉತ್ತಮ ಉದಾಹರಣೆಯೆಂದರೆ ಶ್ರೀಯುತ ಶರ್ಮರು. ಭಾರತದೇಶದ ನೈಋತ್ಯ ಭಾಗದಲ್ಲಿ ದೇಶದ ಯಜಮಾನನೋ ಎಂಬಂತೆ ರಾರಾಜಿಸುತ್ತಿರುವ ರಾಜ್ಯವಾದ ಕರ್ನಾಟಕದಲ್ಲಿ ಮಲೆನಾಡಿನಿಂದಲೇ ಪ್ರಸಿದ್ಧಿಯನ್ನು ಹೊಂದಿದ ಶಿವಮೊಗ್ಗ ಜಿಲ್ಲೆಯ ಸೊರಬ ತಾಲ್ಲೂಕಿನ ನಡಹಳ್ಳಿಯು ಇವರ ಜನ್ಮಸ್ಥಳ. ಭಗವತ್ಪಾದಶಂಕರಾಚಾರ್ಯರು ಜಗಕವತರಿಸಿದ ಪರಮಪವಿತ್ರದಿನವಾದ ವೈಶಾಖಶುದ್ಧ ಪಂಚಮಿಯಂದು ಹಾಗೂ ಮರ್ಯಾದಾ ಪುರುಷೋತ್ತಮನಾದ ಶ್ರೀರಾಮಚಂದ್ರನು ಆವಿರ್ಭವಿಸಿದ ಪುಣ್ಯನಕ್ಷತ್ರವಾದ ಪುನರ್ವಸು ನಕ್ಷತ್ರದಲ್ಲಿ ಶ್ರೀಮತಿ ಜಾನಕಮ್ಮ ತಿಮ್ಮಪ್ಪ ದಂಪತಿಗಳ ಸುಪುತ್ರರಾಗಿ ೦೭-೦೪-೧೯೧೬ ರಂದು ಜನಿಸಿದರು. ವಿಶ್ವಾಮಿತ್ರ ಗೋತ್ರದ ಬೋಧಾಯನ ಸೂತ್ರದ ಯಜುರ್ವೇದಿಗಳಾದ ಶ್ರೀಯುತರು ಜಗದ್ಗುರು ಶಂಕರಾಚಾರ್ಯ ಮಹಾಸಂಸ್ಥಾನಮ್ – ಶ್ರೀಸಂಸ್ಥಾನ ಗೋಕರ್ಣ – ಶ್ರೀರಾಮಚಂದ್ರಾಪುರ ಮಠದ ಪಾರಂಪರಿಕ ಶಿಷ್ಯಪರಂಪರೆಗೆ ಸೇರಿದ ಸ್ಮಾರ್ತ ಬ್ರಾಹ್ಮಣ ಪಂಗಡಗಳಲ್ಲೊಂದಾದ ಹವ್ಯಕ ಪಂಗಡಕ್ಕೆ ಸೇರಿದವರು. <a href="" >ಮುಂದೆ ಓದಿ...</a>
+		</div>
+		<div class="col-md-4">
+			<img src="<?=IMAGE_URL?>bioPic.jpg" alt="ರಂಗನಾಥಶರ್ಮಾ">
+		</div>
+	</div>
+	<div class="row mt-5 awards">
+		<div class="col-md-12 subtitle">
+			<h4 class="text-center">ಪ್ರಶಸ್ತಿಗಳು ಮತ್ತು ಸಮ್ಮಾನಗಳು</h4>
+		</div>
+		<div class="col-md-6">
+			<img src="<?=IMAGE_URL?>awards2.jpg" alt="ರಂಗನಾಥಶರ್ಮಾ">
+			<img src="<?=IMAGE_URL?>awards3.jpg" alt="ರಂಗನಾಥಶರ್ಮಾ">
+			<img src="<?=IMAGE_URL?>awards1.jpg" alt="ರಂಗನಾಥಶರ್ಮಾ">
+		</div>
+		<div class="col-md-6">
+			ಭರತಖಂಡ ಕಂಡ ಅತಿವಿಶಿಷ್ಟ ಪಂಡಿತರು ಶ್ರೀಯುತ ಶರ್ಮರು. ಇವರ ಸಾಧನೆಯನ್ನು ಕಂಡು ಅರಸಿ ಬಂದ ಪ್ರಶಸ್ತಿಗಳು ನೂರಾರು. ನಡಹಳ್ಳಿಯ ಗ್ರಾಮಸ್ಥರ ಪ್ರೀತಿಯ ಸಮ್ಮಾನದಿಂದಾರಂಭಿಸಿ ರಾಷ್ಟ್ರಪತಿ ಪುರಸ್ಕಾರದವರೆಗಿನ ಎಲ್ಲ ಪ್ರಶಸ್ತಿಗಳೂ ಇವರಿಗೆ ಸಂದಿವೆ. ಅನೇಕ ಮಠಗಳು, ವಿಶ್ವವಿದ್ಯಾಲಯಗಳು, ಸಾರ್ವಜನಿಕ ಸಂಸ್ಥೆಗಳು, ಸರಕಾರ ಇವರಿಗೆ ಪ್ರಶಸ್ತಿ ನೀಡುವ ಮೂಲಕ ತಮ್ಮ ಸಂಸ್ಥಯ ಗೌರವವನ್ನು ಹೆಚ್ಚಿಸಿಕೊಂಡಿವೆ. <a href="" >ಇನ್ನಷ್ಟು ನೋಡಿ...</a>
+		</div>
+	</div>
+	<div class="row mt-5 contact">
+		<div class="col-md-12 subtitle">
+			<h4 class="text-right">ನಮ್ಮ ಸಂಪರ್ಕ</h4>
+		</div>
+		<div class="col-md-5">
+			ಡಾ॥ ರಾಮಕೃಷ್ಣ ಭಟ್ ಕೆ<br />
+			ಸಂಯೋಜಕರು<br />
+			ಮಹಾಮಹೋಪಾಧ್ಯಾಯ ವಿದ್ವಾನ್ ಎನ್. ರಂಗನಾಥಶರ್ಮಾ<br />
+			ಅಂತರ್ಜಾಲತಾಣ ನಿರ್ಮಾಣ ಸಮಿತಿ<br />
+			ನಂ : ೧೬೬/ಎ, ನೆಲಮಹಡಿ, ಮೊದಲನೆಯ ಮನೆ,<br />
+			೧೩ನೆಯ ಅಡ್ಡರಸ್ತೆ, ಗಿರಿನಗರ ಮೂರನೆಯ ಹಂತ<br />
+			ಬೆಂಗಳೂರು - ೫೬೦೦೮೫<br />
+			ಸಂಪರ್ಕ ಸಂಖ್ಯೆ : ೦೯೮೪೪೭೪೧೮೫೫<br />
+			ಮಿಂಚಂಚೆ : <span class="en">vidwannrs@gmail.com</span>
+		</div>
+		<div class="col-md-7">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.442278507226!2d77.53695091437494!3d12.943526590874296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3e1535a7b539%3A0x64b47bc9a0325ae8!2s13th+Cross+Rd%2C+Banashankari+Stage+I%2C+Banashankari%2C+Bengaluru%2C+Karnataka!5e0!3m2!1sen!2sin!4v1518611968593" width="600" height="325" frameborder="0" allowfullscreen=""></iframe>
+		</div>
+	</div>
+</div>
+<br/>
+<br/><br/><br/><br/><br/><br/><br/><br/>
