a287de15798b43f057349a3eed38a183ab7f63dd
Suresh <soori_rozee@yahoo.com>
2019-05-06 15:20:59 +0530

    Collaged images added fro articles page

--- application/views/flat/Krutigalu/index.php
+++ application/views/flat/Krutigalu/index.php
@@ -1,54 +1,59 @@
 <div class="container-fluid flat-page firstDiv">
 	<div class="row justify-content-center overlay">
 		<div class="col-md-12 align-self-center">
-			<div class="container">
-				<h2>ಕೃತಿಗಳು</h2><br />
-				<div class="row">
-					<div class="card-columns">
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>Krutigalu/001">
-								<div class="card-body text-center">ಸ್ವತಂತ್ರ ಗ್ರಂಥಗಳು</div>
-							</a>
-						</div>
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>Krutigalu/002">
-								<div class="card-body text-center">ಅನುವಾದ ಗ್ರಂಥಗಳು</div>
-							</a>
-						</div>
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>Krutigalu/003">
-								<div class="card-body text-center">ವ್ಯಾಖ್ಯಾನ ಗ್ರಂಥಗಳು</div>
-							</a>
-						</div>
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>Krutigalu/004">
-								<div class="card-body text-center">ಸಂಪಾದಿತ ಗ್ರಂಥಗಳು</div>
-							</a>
-						</div>  
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>Krutigalu/005">
-								<div class="card-body text-center">ಸಹಕರ್ತೃ ಗ್ರಂಥಗಳು</div>
-							</a>
-						</div>
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>Krutigalu/006">
-								<div class="card-body text-center">ಲೇಖನಗಳು</div>
-							</a>
-						</div>
-						<!-- <div class="card bg-light">
-							<a href="<?=BASE_URL?>Krutigalu/007">
-								<div class="card-body text-center">ಇತರ ಗ್ರಂಥಗಳು</div>
-							</a>
-						</div> -->
-						<div class="card bg-light">
-							<a href="<?=BASE_URL?>Krutigalu/008">
-								<div class="card-body text-center">ಭಾಷಾಂತರಗೊಂಡ ಗ್ರಂಥಗಳು</div>
-							</a>
-						</div>
+			<div class="container bio">
+				<h2 class="text-center my-3">ಕೃತಿಗಳು</h2>
+				<div class="row kruthigalu">
+					<div class="col-md-3 text-center my-3">
+						<a href="<?=BASE_URL?>Krutigalu/001">
+							<img class="img-fluid" src="<?=IMAGE_URL?>kruthigalu/kruthi-1.jpg" />
+							<div class="card-title text-center">ಸ್ವತಂತ್ರ ಗ್ರಂಥಗಳು</div>
+						</a>
+					</div>
+					<div class="col-md-3 text-center my-3">
+						<a href="<?=BASE_URL?>Krutigalu/002">
+							<img class="img-fluid" src="<?=IMAGE_URL?>kruthigalu/kruthi-4.jpg" />
+							<div class="card-title text-center">ಅನುವಾದ ಗ್ರಂಥಗಳು</div>
+						</a>
+					</div>
+					<div class="col-md-3 text-center my-3">
+						<a href="<?=BASE_URL?>Krutigalu/003">
+							<img class="img-fluid" src="<?=IMAGE_URL?>kruthigalu/kruthi-7.jpg" />
+							<div class="card-title text-center">ವ್ಯಾಖ್ಯಾನ ಗ್ರಂಥಗಳು</div>
+						</a>
+					</div>
+					<div class="col-md-3 text-center my-3">
+						<a href="<?=BASE_URL?>Krutigalu/004">
+							<img class="img-fluid" src="<?=IMAGE_URL?>kruthigalu/kruthi-2.jpg" />
+							<div class="card-title text-center">ಸಂಪಾದಿತ ಗ್ರಂಥಗಳು</div>
+						</a>
+					</div>  
+					<div class="col-md-3 text-center my-3">
+						<a href="<?=BASE_URL?>Krutigalu/005">
+							<img class="img-fluid" src="<?=IMAGE_URL?>kruthigalu/kruthi-5.jpg" />
+							<div class="card-title text-center">ಸಹಕರ್ತೃ ಗ್ರಂಥಗಳು</div>
+						</a>
+					</div>
+					<div class="col-md-3 text-center my-3">
+						<a href="<?=BASE_URL?>Krutigalu/006">
+							<img class="img-fluid" src="<?=IMAGE_URL?>kruthigalu/kruthi-3.jpg" />
+							<div class="card-title text-center">ಲೇಖನಗಳು</div>
+						</a>
+					</div>
+					<!-- <div col-md-3-light text-center m-3 my-3">
+						<a href="<?=BASE_URL?>Krutigalu/007">
+							<img class="img-fluid" src="<?=IMAGE_URL?>kruthigalu/kruthi-.jpg" />
+							<div class="card-title text-center">ಇತರ ಗ್ರಂಥಗಳು</div>
+						</a>
+					</div> -->
+					<div class="col-md-3 text-center my-3">
+						<a href="<?=BASE_URL?>Krutigalu/008">
+							<img class="img-fluid" src="<?=IMAGE_URL?>kruthigalu/kruthi-6.jpg" />
+							<div class="card-title text-center">ಭಾಷಾಂತರಗೊಂಡ ಗ್ರಂಥಗಳು</div>
+						</a>
 					</div>
 				</div>
 			</div>
 		</div>
-		<a class="homepage scrollMore goTo" data-destination="#wordOfTheDay"><span></span>&nbsp;</a>
 	</div>
 </div>
