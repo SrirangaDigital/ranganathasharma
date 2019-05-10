5bc0d86e3d6cce66abb9cdc2aebcd076f7f4bc05
Suresh <soori_rozee@yahoo.com>
2019-05-10 16:25:05 +0530

    SUpporting function done

--- application/controllers/data.php
+++ application/controllers/data.php
@@ -7,93 +7,32 @@ class data extends Controller {
 		parent::__construct();
 	}
 
-	public function index() {
+	public function header(){
 
-		$this->insertDetails();
-	}
+		$htmlFIles = $this->model->getFilesIteratively(PHY_FLAT_URL . '3-Books/', $pattern = '/xhtml$/i');
 
-	public function insertMetaData(){
+		echo "<ul>";
 
-		$metaData = $this->model->getMetadaData();
-		
-		$this->model->db->createDB(DB_NAME, DB_SCHEMA);
-
-		$dbh = $this->model->db->connect(DB_NAME);
-
-		$this->model->db->dropTable(METADATA_TABLE, $dbh);
-
-		$this->model->db->createTable(METADATA_TABLE, $dbh, METADATA_TABLE_SCHEMA);
-
-		foreach ($metaData as $row)
-		{
-			$this->model->db->insertData(METADATA_TABLE, $dbh, $row);
-		}
-	}
-
-	public function modify(){
-
-		$xhtmlFiles = $this->model->getFilesIteratively(PHY_FLAT_URL . '3-Books/' , $pattern = '/xhtml$/i');
-		
-		foreach ($xhtmlFiles as $file) {
+		foreach ($htmlFIles as $file) {
 			
-			$bookID = preg_replace('/.*\/(.*)\.xhtml/', "$1", $file);
-			$content = preg_split("/\n/", file_get_contents($file));
+			$data = file_get_contents($file);
+			$data = preg_split('/\n/', $data);
+			foreach ($data as $line) {
 
-			foreach ($content as $line) {
+				if(preg_match('/<div data-bcode="(.*?)">/', $line, $matches)){
+					$bookID = $matches[1];
+				}
 
-				if(preg_match('/<li page="(.*?)-(.*?)">(.*)<\/li>/', $line, $matches)){
-					$pageNumber = $this->getRelativePage($bookID, $matches[1]);
-					if($pageNumber == 'Not Found'){
-						echo $bookID . "-->" . $line . "\n";
-					}
+				if(preg_match('/<h1 class="title" data-language="(.*?)">(.*)<\/h1>/', $line, $matches)) {
+
+					echo '<li bookid="' . $bookID . '" language="' . $matches[1] . '">' . $matches[2] . "</li>\n";
+					continue;
 				}
 			}
 		}
+
+		echo "</ul>";
 	}
-
-	public function getRelativePage($bookID, $page) {
-
-		$path = PHY_PUBLIC_URL . 'Text/' . $bookID . '/';
-		$pages = glob($path . '*.txt');
-
-		sort($pages, SORT_STRING);
-		$relativePage = array_search(PHY_PUBLIC_URL . 'Text/' . $bookID . '/' . $page . '.txt', $pages);
-
-		$relativePage = ($relativePage !== false) ? $relativePage + 1 : "Not Found";
-
-		return $relativePage;
-	}
-
-	public function generateCopyrightJson(){
-
-		$csvFile = PHY_PUBLIC_URL . 'csv/metadata.tsv';
-		$content = file($csvFile);
-		$data = [];
-
-		foreach ($content as $line) {
-			
-			$tmp = [];
-			$split = preg_split('/\t/', $line);
-
-			$BookId = $split[0];
-			//$tmp['Title'] = $split[1];
-			//$tmp['Language'] = $split[2];
-			$tmp['Year'] = $split[3];
-			$tmp['Rs'] = $split[4];
-			$tmp['Pages'] = $split[5];
-			$tmp['Author'] = $split[7];
-			$tmp['Available'] = $split[6];
-			$tmp['Translator'] = $split[8];
-			$tmp['Publisher'] = $split[9];
-			$tmp['Phone'] = $split[10];
-			if($split[11] != '')
-			$tmp['Copyrights'] = '© ' . $split[11];
-			$tmp['Edition'] = $split[12];
-			$tmp['Category'] = trim($split[13]);
-			$data[$BookId] = array_filter($tmp);
-		}
-
-		file_put_contents(PHY_BASE_URL . 'json-precast/metadata.json', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
-	}
+	
 }
 ?>
