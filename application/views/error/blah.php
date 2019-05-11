5fb0dfcc42d3c563ffb6609ad67258c280943e40
Suresh <soori_rozee@yahoo.com>
2019-05-11 11:48:41 +0530

    UI Fixes for error pages

--- application/views/error/blah.php
+++ application/views/error/blah.php
@@ -1,3 +1,8 @@
-<h1>Sorry!</h1>
-<h2>&nbsp;</h2>
-<p>Something went wrong! Please try again after some time.</p>
+<div class="row text-center">
+	<div class="col-md-12 my-5">
+		<h1>Sorry!</h1>
+		<h2>&nbsp;</h2>
+		<p>Something went wrong! Please try again after some time.</p>
+	</div>
+</div>
+
--- application/views/error/prompt.php
+++ application/views/error/prompt.php
@@ -1,3 +1,7 @@
-<h1>Request Unsuccessful!</h1>
-<h2>&nbsp;</h2>
-<p><?=$data['msg']?></p>
+<div class="row text-center">
+	<div class="col-md-12 my-5">
+		<h1>Request Unsuccessful!</h1>
+		<h2>&nbsp;</h2>
+		<p><?=$data['msg']?></p>
+	</div>
+</div>
